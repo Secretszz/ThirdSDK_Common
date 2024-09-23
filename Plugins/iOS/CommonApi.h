41168c7dc769bbdce3db2312cdac459b1c84a83c
SecretsZhong <383426620@qq.com>
2024-09-23 11:47:24 +0800

    update

--- Plugins/iOS/CommonApi.h
+++ Plugins/iOS/CommonApi.h
@@ -11,3 +11,11 @@
 typedef void(* U3DBridgeCallback_Success)(const char *);
 typedef void(* U3DBridgeCallback_Cancel)();
 typedef void(* U3DBridgeCallback_Error)(int, const char *);
+
+@interface CommonApi : NSObject
++(NSString *)objectToJson:(id)obj;
++(id)jsonToObject:(NSString *)json;
++(NSDictionary*)jsonToNSDictionary:(NSString *)json;
++(id)perseJsonObjectWitchDictionary:(NSDictionary*)dic clz:(Class)clz;
++(id)perseJsonToObject:(Class)clz jsonStr:(NSString*)jsonString;
+@end
